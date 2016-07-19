f17a95964163d873996fe7cb944ef92b260baf28
Nima M.Farahani <nima_f101@yahoo.com>
2016-07-19 01:56:31 -0400

    first commit
    creating the UI

---
+++ weather/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Nima Farahani on 7/19/16.
+//  Copyright © 2016 Nima Farahani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var userCity: UITextField!
+    
+    @IBAction func findWeather(sender: AnyObject) {
+    }
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
